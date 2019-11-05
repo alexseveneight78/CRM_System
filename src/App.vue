b5e993060c4ac2e5835068b608aa653c7b5ce1e9
Alexandr Abramtchik <alexseveneight@gmail.com>
2019-11-05 12:32:25 +0300

    'in_the_very_beginning'

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="app">
+    <header>
+      <app-navigation></app-navigation>
+    </header>
+    <hr>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import Home from './components/Home';
+  import Navigation from './components/Navigation';
+
+
+
+  export default {
+    components: {
+      appHome: Home,
+      appNavigation: Navigation
+    },
+    data () {
+      return {
+        
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .app {
+    text-align: center;
+
+    header {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: nowrap;
+    }
+  }
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="navigation">
+        <ul>
+            <router-link to="/customers" tag="li"><a href="">Customers</a></router-link>
+            <router-link to="/carriers" tag="li"><a href="">Carriers</a></router-link>
+            <router-link to="/orders" tag="li"><a href="">Orders</a></router-link>
+            <router-link to="/requests" tag="li"><a href="">Requests</a></router-link>
+            <router-link to="/company-structure" tag="li"><a href="">Company structure</a></router-link>
+            <router-link to="/salary-motivation" tag="li"><a href="">Salary & Motivation</a></router-link>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    #navigation {
+        width: 100%;
+        background-color: 	rgb(153, 102, 102);
+
+            ul {
+                padding: 0;
+                margin: 5px;
+                display: flex;
+                align-items: center;
+                flex-direction: row;
+                justify-content: space-around;
+
+                li {
+                    padding: 0;
+                    margin-left: 15px;
+                    list-style: none;
+
+                    a {
+                        text-decoration: none;
+                        color: #fff;
+                        display: block;
+                        padding: 3px 8px;
+                        transition: 400ms;
+                        border: 1px solid #fff;
+                        &:hover {
+                            color: #000;
+                            background-color: #fff;
+                        }
+                    }
+
+                }
+    }
+    }
+
+</style>
