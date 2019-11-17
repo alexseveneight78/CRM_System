9bf66058399398b40a491e30254198a3826fa3d9
Alexandr Abramtchik <alexseveneight@gmail.com>
2019-11-17 21:13:32 +0300

    '...'

--- src/components/Company structure/employees/Employee.vue
+++ src/components/Company structure/employees/Employee.vue
@@ -1,17 +1,49 @@
 <template>
   <div class="employee">
     <h4>Employee component</h4>
-    <p>Loaded ID: {{ $route.params.id }}</p>
+    <button @click="editEmployee">Edit data</button>
+    <button @click="saveEditedData">Save edited data</button>
+    <p>
+      Loaded ID:
+      <span class="edit_employee">{{ $route.params.id }}</span>
+    </p>
     <ul>
-      <li>First Name: {{ $route.params.firstName }}</li>
-      <li>Last Name: {{ $route.params.lastName }}</li>
-      <li>Date of employment: {{ $route.params.employmentDate }}</li>
-      <li>Email: {{ $route.params.email }}</li>
-      <li>Mobile phone: {{ $route.params.mobile }}</li>
-      <li>Salary netto: {{ $route.params.salaryNetto }}$</li>
-      <li>Salary brutto: {{ $route.params.salaryBrutto }}$</li>
-      <li>Department: {{ $route.params.department }}</li>
-      <li>Position: {{ $route.params.position }}</li>
+      <li>
+        First Name:
+        <span class="edit_employee">{{ $route.params.firstName }}</span>
+      </li>
+      <li>
+        Last Name:
+        <span class="edit_employee">{{ $route.params.lastName }}</span>
+      </li>
+      <li>
+        Date of employment:
+        <span class="edit_employee">{{ $route.params.employmentDate }}</span>
+      </li>
+      <li>
+        Email:
+        <span class="edit_employee">{{ $route.params.email }}</span>
+      </li>
+      <li>
+        Mobile phone:
+        <span class="edit_employee">{{ $route.params.mobile }}</span>
+      </li>
+      <li>
+        Salary netto:
+        <span class="edit_employee">{{ $route.params.salaryNetto }}$</span>
+      </li>
+      <li>
+        Salary brutto:
+        <span class="edit_employee">{{ $route.params.salaryBrutto }}$</span>
+      </li>
+      <li>
+        Department:
+        <span class="edit_employee">{{ $route.params.department }}</span>
+      </li>
+      <li>
+        Position:
+        <span class="edit_employee">{{ $route.params.position }}</span>
+      </li>
     </ul>
   </div>
 </template>
@@ -26,6 +58,16 @@ export default {
   },
   updated() {
     console.log("Component Employee is updated!", this.$route.params);
+  },
+  methods: {
+    editEmployee() {
+      let data = document.querySelectorAll(".edit_employee");
+      console.log(typeof data);
+      for (let key in data) {
+        data[key].contentEditable = true;
+      }
+    },
+    saveEditedData() {}
   }
   //   watch: {
   //     $route(to, from) {
@@ -40,6 +82,10 @@ export default {
   border: 1px solid salmon;
   margin: 5px 10px;
   padding: 15px;
+
+  span {
+    font-weight: bold;
+  }
 
   ul {
     display: inline-block;
